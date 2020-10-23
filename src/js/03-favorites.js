b9f13dcb5ad0ef49c4faee706d486ecb803959f0
Leire Ordeñana <leireomadina@gmail.com>
2020-10-23 14:08:36 +0200

    Add logic to push a selected item into the array

--- src/js/03-favorites.js
+++ src/js/03-favorites.js
@@ -7,9 +7,20 @@ function saveFavorite(event) {
   //gets the id of each selected item 
   const selectedShow = event.currentTarget.id;
   console.log(selectedShow);
+  //searchs if the item is already in the array (different from -1)
+  const isFavorite = favoritedShows.indexOf(selectedShow);
+  console.log(isFavorite);
+ if (isFavorite === -1) {
   //push that item into the array
+  console.log("Lo meto en el array");
   favoritedShows.push(selectedShow);
   console.log(favoritedShows);
+ } else {
+  console.log("Lo quito del array");
+  console.log(favoritedShows);
+
+ }
+ 
 }
 
 function listenShows() {
