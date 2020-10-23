9de4677697672e1b3e2163fba75804e985a895b4
Leire Ordeñana <leireomadina@gmail.com>
2020-10-23 09:51:24 +0200

    Include fetch in a function and add listener

--- src/js/01-api.js
+++ src/js/01-api.js
@@ -3,46 +3,33 @@
 // Get elements and define tv-series array
 
 const searchInput = document.querySelector(".js-search");
-const tvSeriesContainer = document.querySelector(".js-series-container");
+const showsContainer = document.querySelector(".js-shows-container");
 const btn = document.querySelector(".js-btn");
-// let tvSeries = [];
 let searchedShows = [];
+//add variable to store API base url?
 
-// Fetch
+// Fetch function
 
-fetch("http://api.tvmaze.com/search/shows?q=girls")
+function getData() {
+  const userSearch = searchInput.value;
+  fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`)
   .then(function(response) {
     // console.log(response);
     return response.json();
   })
   .then(function(data){
-    //
+    //loop to get the name and img of each show
     for (let i = 0; i < data.length; i++) {
-      searchedShows = data[i];
-      console.log(searchedShows);
+      // searchedShows = data[i];
+      // console.log(searchedShows);
       console.log(data[i].show.name);
       console.log(data[i].show.image.medium);
     }
   })
-
-/*
-function getData() {
-  const userSearch = searchInput.value;
-  console.log(userSearch);
-  fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`)
-    .then(function (response) {
-      console.log(response);
-    })
-    // .then((response) => response.json);
-    .then(function (data) {
-      console.log("información", data);
-      // tvSeries = data.show;
-      // console.log(tvSeries);
-      // paintCards();
-    });
 }
 
-// getData();
+// Event listener and function calling
+
 btn.addEventListener("click", getData);
 
-*/
+getData();
