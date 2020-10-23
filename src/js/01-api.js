2963509d4d37277568b0c886e981a6828884e5a1
Leire Ordeñana <leireomadina@gmail.com>
2020-10-23 12:22:04 +0200

    Move paint function into a different JS file

--- src/js/01-api.js
+++ src/js/01-api.js
@@ -1,9 +1,8 @@
-//** API related JS code
+//** API 
 
 // Get elements and define tv-series array
 
 const searchInput = document.querySelector(".js-search");
-const showsContainer = document.querySelector(".js-shows-container");
 const btn = document.querySelector(".js-btn");
 let searchedShows = [];
 
@@ -19,34 +18,6 @@ function getData() {
       // console.log(searchedShows);
       paintShows();
     });
-}
-
-// Paint function
-
-function paintShows() {
-  let htmlShow = "";
-  // let showItem = {};
-  //loop to get the name and img of each show
-  for (let i = 0; i < searchedShows.length; i++) {
-    //POSIBLE MEJORA: crear un objeto vacío??????
-    //MEJORA: usar DOM avanzado
-    // console.log(searchedShows[i].show.name);
-    // console.log(searchedShows[i].show.image.medium);
-    // let showImage = searchedShows[i].show.image.medium;
-    const showName = searchedShows[i].show.name;
-    htmlShow += `<li style="background-color: #ccc" class="shows-list__item" id="${i}">`;
-    htmlShow += `<h2 class="shows-list__title">${showName}</h2>`;
-    htmlShow += `</li>`;
-    if (searchedShows[i].show.image != null) {
-      let imgURL = searchedShows[i].show.image.medium;
-      htmlShow += `<img src="${imgURL}" alt="Imagen de la serie ${showName}" style="height: 200px" class="shows-list__img" title="Imagen de la serie ${showName}"></img>`;
-    } else {
-      imgURL =
-        "https://via.placeholder.com/210x295/ffffff/666666/?";
-      htmlShow += `<img src="${imgURL}" alt="Imagen de la serie ${showName}" style="height: 200px" class="shows-list__img`;
-    }
-  }
-  showsContainer.innerHTML = htmlShow;
 }
 
 // Event listener and function calling
--- src/js/02-paint.js
+++ src/js/02-paint.js
@@ -1,1 +1,28 @@
-// Paint function
+//** Paint function 
+const showsContainer = document.querySelector(".js-shows-container");
+
+function paintShows() {
+  let htmlShow = "";
+  // let showItem = {};
+  //loop to get the name and img of each show
+  for (let i = 0; i < searchedShows.length; i++) {
+    //MEJORA 1: usar DOM avanzado
+    //MEJORA 2: crear un objeto vacío ¿?
+    // console.log(searchedShows[i].show.name);
+    // console.log(searchedShows[i].show.image.medium);
+    // let showImage = searchedShows[i].show.image.medium;
+    const showName = searchedShows[i].show.name;
+    htmlShow += `<li style="background-color: #ccc" class="shows-list__item" id="${i}">`;
+    htmlShow += `<h2 class="shows-list__title">${showName}</h2>`;
+    htmlShow += `</li>`;
+    if (searchedShows[i].show.image != null) {
+      let imgURL = searchedShows[i].show.image.medium;
+      htmlShow += `<img src="${imgURL}" alt="Imagen de la serie ${showName}" style="height: 200px" class="shows-list__img" title="Imagen de la serie ${showName}"></img>`;
+    } else {
+      imgURL =
+        "https://via.placeholder.com/210x295/ffffff/666666/?";
+      htmlShow += `<img src="${imgURL}" alt="Imagen de la serie ${showName}" style="height: 200px" class="shows-list__img`;
+    }
+  }
+  showsContainer.innerHTML = htmlShow;
+}
