473700b803a2a493b4beba794b74dafd7c26241b
leireomadina <leireomadina@gmail.com>
2022-10-12 16:00:29 +0200

    Refactor createParagraph function

--- src/js/01-api-search.js
+++ src/js/01-api-search.js
@@ -2,69 +2,84 @@
 
 // Prevents unnecessary API requests
 const handleSearch = () => {
-  if (searchInput.value === "") {
-    handleEmptySearch();
-  } else {
-    getDataFromApi();
-  }
+	if (searchInput.value === '') {
+		handleEmptySearch();
+	} else {
+		getDataFromApi();
+	}
 };
 
 // TVMaze API connection
-const getDataFromApi = async() => {
-  const ENDPOINT = "//api.tvmaze.com/search/shows?q=";
-  const userSearch = searchInput.value;
+const getDataFromApi = async () => {
+	const ENDPOINT = '//api.tvmaze.com/search/shows?q=';
+	const userSearch = searchInput.value;
 
-  try {
-    const resp = await fetch(`${ENDPOINT} + ${userSearch}`);
-    if(!resp.ok) throw "Im sorry, there has been a server error. Try again later!";
-    const data = await resp.json();
-    if (Object.entries(data).length === 0) {
-      handleNotFound();
-    } else {
-      searchedShows = data;
-      resultsContainer.classList.add('hide');
-      tipsContainer.classList.remove('hide');
-      tipsContainer.innerHTML = '<p>Select a TV show to add it as favorite:</p>';
-      createParagraph("First you need to write the name of a TV show :)");
-      paintShows();
-      createParagraph("");
-      listenShows();
-      listenFavs();
-    }
-  } catch (error) {
-    handleServerError(error);
-  }
-
+	try {
+		const resp = await fetch(`${ENDPOINT} + ${userSearch}`);
+		if (!resp.ok)
+			throw 'Im sorry, there has been a server error. Try again later!';
+		const data = await resp.json();
+		if (Object.entries(data).length === 0) {
+			handleNotFound();
+		} else {
+			searchedShows = data;
+			resultsContainer.classList.add('hide');
+			tipsContainer.classList.remove('hide');
+			createParagraph(
+				'Select a TV show to add it as favorite:)',
+				tipsContainer
+			);
+			createParagraph(
+				'First you need to write the name of a TV show :)',
+				resultsContainer
+			);
+			createParagraph('', resultsContainer);
+			paintShows();
+			listenShows();
+			listenFavs();
+		}
+	} catch (error) {
+		handleServerError(error);
+	}
 };
 
-const createParagraph = (innerContent) => {
-  const newParagraph = `<p class="error-message">${innerContent}</p>`;
-  return (resultsContainer.innerHTML = newParagraph);
+const createParagraph = (innerContent, outerContainer) => {
+	const newParagraph = `<p class="error-message">${innerContent}</p>`;
+	return (outerContainer.innerHTML = newParagraph);
 };
 
 const handleBackgroundImage = () => {
-  showsContainer.innerHTML = "";
-  pageWrapper.classList.remove("page-wrapper");
-  pageWrapper.classList.remove("hidden-bg");
-  pageWrapper.classList.add("not-found");
+	showsContainer.innerHTML = '';
+	pageWrapper.classList.remove('page-wrapper');
+	pageWrapper.classList.remove('hidden-bg');
+	pageWrapper.classList.add('not-found');
 };
 
 const handleEmptySearch = () => {
-  resultsContainer.classList.remove('hide');
-  createParagraph("First you need to write the name of a TV show :)");
-  handleBackgroundImage();
+	resultsContainer.classList.remove('hide');
+	createParagraph(
+		'First you need to write the name of a TV show :)',
+		resultsContainer
+	);
+	handleBackgroundImage();
 };
 
 const handleNotFound = () => {
-  resultsContainer.classList.remove('hide');
-  createParagraph("Oops, that show is not on our database. Try again!");
-  handleBackgroundImage();
+	resultsContainer.classList.remove('hide');
+	createParagraph(
+		'Oops, that show is not on our database. Try again!',
+		resultsContainer
+	);
+	handleBackgroundImage();
 };
 
 const handleServerError = (error) => {
-  resultsContainer.classList.remove('hide');
-  createParagraph("Im sorry, there has been a server error. Try again later!");
-  handleBackgroundImage();
+	resultsContainer.classList.remove('hide');
+	createParagraph(
+		'Im sorry, there has been a server error. Try again later!',
+		resultsContainer
+	);
+	handleBackgroundImage();
 };
 
-searchButton.addEventListener("click", handleSearch);
+searchButton.addEventListener('click', handleSearch);
